a355db383f4ab9a5bf72bb9853e35b690abe3a5d
Pablo T <pablotapiavasquez@gmail.com>
2020-11-14 14:27:26 -0300

    minortweak

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import { faHeadphones } from '@fortawesome/free-solid-svg-icons';
 const Nav = ( {libraryStatus, setLibraryStatus} ) => {
     return(
         <nav>
-            <h1> <img src="/img/logo.png" className="logo-resize" /> </h1>
+            <h1> <img src="/img/logo.png" alt="vinyllogo" className="logo-resize" /> </h1>
             <button onClick={() => setLibraryStatus(!libraryStatus)}>
                 <FontAwesomeIcon icon={faHeadphones} libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
                 Library
